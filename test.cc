0d5f7db6c552737ffe62858627cf4ca7356a1e67
tian <2431228532@qq.com>
2022-02-19 21:45:41 +0800

    test

---
+++ test.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  for (int i = 0; i < 100 ; i++) {
+    for (int j = 0; j < 100; j++) {
+      int a[][j];
+      int b;
+    }
+  }
+  int b;
+  int c;
+  double d;
+}
